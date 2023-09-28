5e45b2b203e38653b2e6ec77ae89c8c2aabb9140
Arda Bakır <abakir@carex.ai>
2023-09-29 01:22:24 +0300

    Display dead and alive cells on a 2D array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hi");
+        
+        int col;
+        int row;
+        string columns = Console.ReadLine();
+        string rows = Console.ReadLine();
+
+        Int32.TryParse(columns, out col);
+        Int32.TryParse(rows, out row);
+
+        Console.WriteLine(col + " " + row);
+
+        int[,] myArr = new int[col,row];
+
+
+        Random rnd = new Random();
+        int numOfLiveCells = rnd.Next((int)Math.Ceiling(row * col * 0.75));
+        for (int i = 0; i < numOfLiveCells; i++)
+        {
+            int posX = rnd.Next(col);
+            int posY = rnd.Next(row);
+            while (myArr[posX,posY] != 0)
+            {
+                posX = rnd.Next(col);
+                posY = rnd.Next(row);
+            }
+            myArr[posX, posY]++;
+        }
+
+        for (int i = 0; i < myArr.GetLength(0); i++)
+        {
+            for (int j = 0; j < myArr.GetLength(1); j++)
+            {
+                Console.Write(myArr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    
+        Console.ReadKey();
+        Console.ReadLine();
+    }
+}
